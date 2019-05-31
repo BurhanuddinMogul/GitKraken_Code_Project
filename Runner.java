0f984403007a70f9b34274be05c66d21e52fc931
Burhanuddin <bmtech5253@gmail.com>
2019-05-31 12:15:35 -0400

    Runner and interface for perfect square functionality created

--- Runner.java
+++ Runner.java
@@ -1,5 +1,12 @@
 public class Runner{
 	public static void main(String[]args){
-	
+
+		Perfect_Square pS = (int x)-> {
+			double root = Math.sqrt(x);
+			return (root - Math.floor(root) == 0);
+		};
+
+		System.out.printf("%s", ""+pS.isPerfectSquare(3));
+
 	}
 }
